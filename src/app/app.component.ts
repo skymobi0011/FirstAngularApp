43d807ef6461fa72f3eee41c0b9566b21aae710b
HoangBui <HoangBui@HOANG>
2024-11-11 15:21:09 +0700

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { HomeComponent } from './home/home.component';
+import { RouterLink, RouterModule, RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  template: `
+  <main>
+    <header class="brand-name">
+      <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
+    </header>
+    <section class="content">
+      <router-outlet></router-outlet>
+    </section>
+
+  </main>
+  `,
+  styleUrl: './app.component.css',
+  imports: [HomeComponent, RouterModule, RouterLink, RouterOutlet],
+
 })
 export class AppComponent {
-  title = 'FirstAngularApp';
+  title = 'homes';
 }
